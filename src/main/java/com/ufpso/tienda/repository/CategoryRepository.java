d7b8225eeadba4af481a876497c671ed264e8e68
Laura Vanessa Ibanez <143549206+VanePe@users.noreply.github.com>
2023-09-27 15:47:13 -0500

    Creación de la clase categorías y respositorio, además de modificar conexión a bd

---
+++ src/main/java/com/ufpso/tienda/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.ufpso.tienda.model.repository;
+
+import com.ufpso.tienda.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category,Long>{
+
+}
