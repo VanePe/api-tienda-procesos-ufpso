0be286fd70d541e9a51371780ec2835cf2d04961
Breynner Ariza <136642407+BreynnerAriza@users.noreply.github.com>
2023-09-29 21:18:33 -0500

    Implementacion del Metodo actualizar en el ArticleService y implementacion de los metodos en el ArticleController

--- src/main/java/com/ufpso/tienda/controller/ArticleController.java
+++ src/main/java/com/ufpso/tienda/controller/ArticleController.java
@@ -1,4 +1,43 @@
 package com.ufpso.tienda.controller;
 
+import com.ufpso.tienda.model.Article;
+import com.ufpso.tienda.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ArticleController {
+
+    @Autowired
+    private ArticleService articleService;
+
+    @GetMapping("articles/{id}")
+    public ResponseEntity<Article> getArticleById(@PathVariable Long id){
+            return new ResponseEntity<>(articleService.getArticleById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("articles")
+    public ResponseEntity<List<Article>> findAll(){
+        return  new ResponseEntity<>(articleService.findAllArticles(),HttpStatus.OK);
+    }
+
+    @PostMapping("articles")
+    public ResponseEntity<Article> create(@RequestBody Article article){
+        return new ResponseEntity<>(articleService.createArticle(article),HttpStatus.CREATED);
+    }
+
+    @PutMapping("articles/{id}")
+    public ResponseEntity<Article> update(@RequestBody Article articleReq, @PathVariable Long id){
+        return new ResponseEntity<>(articleService.updateArticle(articleReq,id),HttpStatus.OK);
+    }
+
+    @DeleteMapping("articles/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        return new ResponseEntity<>(Boolean.toString(articleService.deleteArticle(id)),HttpStatus.NO_CONTENT);
+    }
+
 }
--- src/main/java/com/ufpso/tienda/service/ArticleService.java
+++ src/main/java/com/ufpso/tienda/service/ArticleService.java
@@ -5,6 +5,7 @@ import com.ufpso.tienda.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,10 +15,6 @@ public class ArticleService {
     @Autowired
     private ArticleRepository articleRepository;
 
-    public Article createArticle(Article article){
-        return articleRepository.save(article);
-    }
-
     public Article getArticleById(Long id){
         return articleRepository.findById(id).get();
     }
@@ -26,6 +23,21 @@ public class ArticleService {
         return (List<Article>) articleRepository.findAll();
     }
 
+    public Article createArticle(Article article){
+        return articleRepository.save(article);
+    }
+
+    public Article updateArticle(Article articleReq,Long id){
+        Optional<Article> articleBd = articleRepository.findById(id);
+        if(articleBd.isEmpty())
+            return null;
+        articleBd.get().setNameArticle(articleReq.getNameArticle());
+        articleBd.get().setDescriptionArticle(articleReq.getDescriptionArticle());
+        articleBd.get().setStock(articleReq.getStock());
+        articleBd.get().setPriceArticle(articleReq.getPriceArticle());
+        articleBd.get().setCategory(articleReq.getCategory());
+        return articleRepository.save(articleBd.get());
+    }
 
     public boolean deleteArticle(Long id){
         Optional<Article> articleBd = articleRepository.findById(id);
